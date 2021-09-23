50798964eb3202218a232a8c596ec67a98f68114
Tani Kaye <1tanikaye@gmail.com>
2021-09-23 13:36:07 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+  var newArray = [];
+for (var i = 0; i < array.length; i++) {
+  newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+}
+return newArray
+}
+
+writeCards(['john', 'lisa', 'mary'])
+
+function countDown(int) {
+var i = int;
+while (i >= 0) {
+  console.log(i);
+  i--
+}
+}
